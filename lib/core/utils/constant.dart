98e8641fe6d5fd110fb53f0978189f459f045f43
Mahmoud <mellebda@student.eelu.edu.eg>
2023-07-15 14:28:53 +0200

    create move model for move done

---
+++ lib/core/utils/constant.dart
@@ -0,0 +1,7 @@
+class AppConstant {
+  static const String domain = "https://api.themoviedb.org/3";
+  static const String apiKey = "992e2fd71b1daca0c72978cd45a84073";
+  static const String popular = "popular";
+  static const String nowPlaying = "now_playing";
+  static const String topRated = "top_rated";
+}
