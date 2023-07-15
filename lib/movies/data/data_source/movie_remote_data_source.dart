6701e2d70b7bf7cc3f6a0057dba596958604f3db
Mahmoud <mellebda@student.eelu.edu.eg>
2023-07-15 15:50:52 +0200

    create Server failure

--- lib/movies/data/data_source/movie_remote_data_source.dart
+++ lib/movies/data/data_source/movie_remote_data_source.dart
@@ -3,24 +3,21 @@ import 'package:flutter/material.dart';
 import 'package:movie_app/core/utils/constant.dart';
 import 'package:movie_app/movies/data/model/movie_model.dart';
 
-class MovieRemoteDataSource{
+class MovieRemoteDataSource {
+  Future<List<MovieModel>> getNowPlayingMovie() async {
+    try {
+      final response = await Dio().get(
+          "${AppConstant.domain}/movie/${AppConstant.nowPlaying}?api_key=${AppConstant.apiKey}");
 
-Future< List<MovieModel>?> getNowPlayingMovie()async{
-  try {
-  final response =await Dio().get("${AppConstant.domain}/movie/${AppConstant.nowPlaying}?api_key=${AppConstant.apiKey}");
+      final List<dynamic> responseDynamic = response.data["results"];
+      final List<MovieModel> responseMovieModel = responseDynamic
+          .map((dataList) => MovieModel.fromjson(dataList))
+          .toList();
 
-  final List<dynamic> responseDynamic = response.data["results"];
-  final List<MovieModel> responseMovieModel = responseDynamic.map((dataList) => MovieModel.fromjson(dataList)).toList();
-
-return responseMovieModel;
-
-} on Exception catch (e) {
-  debugPrint("my error is ${e.toString()}");
-return null;
-}
-
-
-
-}
-
+      return responseMovieModel;
+    } on Exception catch (e) {
+      debugPrint("my error is ${e.toString()}");
+      return [];
+    }
+  }
 }
