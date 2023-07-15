f32a0cf9ac42be6668d094d11c1a04afc367b02b
Mahmoud <mellebda@student.eelu.edu.eg>
2023-07-15 15:00:01 +0200

    create entities for move

---
+++ lib/movies/domain/entities/movie.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:equatable/equatable.dart';
+
+class Movie extends Equatable {
+  final int id;
+  final String title;
+  final String backdropPath;
+  final List<int> genreIds;
+  final String overview;
+  final double voteAverage;
+
+ const Movie({
+    required this.id,
+    required this.title,
+    required this.backdropPath,
+    required this.genreIds,
+    required this.overview,
+    required this.voteAverage,
+  });
+
+  @override
+  List<Object> get props {
+    return [
+      id,
+      title,
+      backdropPath,
+      genreIds,
+      overview,
+      voteAverage,
+    ];
+  }
+}
